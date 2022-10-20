a74c50d0f69e0c4febbc0f10cf6feda044b639ee
Nibeza kevin <kevin.nibeza@gmail.com>
2022-10-20 21:45:33 +0200

    android version star+

---
+++ android/app/src/main/java/com/starplus/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.starplus.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.starplus.com',
+  appName: 'Star+',
+  webDir: 'build/',
+  bundledWebRuntime: false
+};
+
+export default config;
